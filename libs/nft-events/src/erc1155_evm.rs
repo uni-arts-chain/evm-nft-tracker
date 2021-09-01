463a56eb9094d3015435be78d39b778bee061a59
Aki Wu <wuminzhe@gmail.com>
2021-09-01 09:49:44 +0800

    add balances to erc1155 event

--- libs/nft-events/src/erc1155_evm.rs
+++ libs/nft-events/src/erc1155_evm.rs
@@ -1,7 +1,7 @@
 //! This module is a library to get ERC1155 transfer events.
-use crate::{EvmClient, Result};
+use crate::{EvmClient, Result, erc1155_db::add_collection_to_db};
 use array_bytes::hex2bytes_unchecked as bytes;
-use web3::types::{Bytes, Log, H160, H256, U256};
+use web3::{ethabi::token, types::{Bytes, Log, H160, H256, U256}};
 
 /// The Erc721 Transfer Event Wrapper
 #[derive(Debug, Clone)]
@@ -16,8 +16,12 @@ pub struct Erc1155Event {
     pub operator: H160,
     /// Transfer from
     pub from: H160,
+    /// Balance of from
+    pub balance_of_from: U256,
     /// Transfer to
     pub to: H160,
+    /// Balance of to
+    pub balance_of_to: U256,
     /// The token type being transferred
     pub token_id: U256,
     /// Number of the token transferred
@@ -40,23 +44,105 @@ pub async fn get_erc1155_events(
     let topics = vec![transfer_single_topic, transfer_batch_topic];
     let logs = client.get_logs(None, topics, from, to).await?;
 
-    let mut events = vec![];
+    let mut result = vec![];
 
     for log in logs {
         if client.is_visual_erc1155(log.address).await? {
             if log.topics[0] == transfer_single_topic {
-                let token_id = U256::from_big_endian(&log.data.0[0..32]);
-                let amount = U256::from_big_endian(&log.data.0[32..64]);
-                events.push(build_event(&log, token_id, amount));
+                let event = build_event(client, &log).await?;
+                result.push(event);
             } else {
-                let (token_ids, amounts) = get_ids_and_amounts(&log.data);
-                for i in 0..token_ids.len() {
-                    let token_id = token_ids[i];
-                    let amount = amounts[i];
-                    events.push(build_event(&log, token_id, amount));
-                }
+                let mut events = build_events(client, &log).await?;
+                result.append(&mut events);
             };
         }
+    }
+
+    Ok(result)
+}
+
+async fn build_event(client: &EvmClient, log: &Log) -> Result<Erc1155Event> {
+    let token_id = U256::from_big_endian(&log.data.0[0..32]);
+    let amount = U256::from_big_endian(&log.data.0[32..64]);
+    let block_number = log.block_number.map(|b| b.as_u64());
+    let address = log.address;
+    let transaction_hash = log.transaction_hash;
+    let operator = H160::from(log.topics[1]);
+    let from = H160::from(log.topics[2]);
+    let to = H160::from(log.topics[3]);
+    let balances = client.get_erc1155_balances(&address, &vec![from, to], &vec![token_id, token_id], block_number).await?;
+    // let balance_of_from = U256::zero();// balances[0];
+    // let balance_of_to = U256::zero();// balances[1];
+    let balance_of_from = balances[0];
+    let balance_of_to = balances[1];
+
+    Ok(
+        Erc1155Event {
+            block_number,
+            address,
+            transaction_hash,
+            operator,
+            from,
+            balance_of_from,
+            to,
+            balance_of_to,
+            token_id, 
+            amount,
+        }
+    )
+}
+
+async fn build_events(client: &EvmClient, log: &Log) -> Result<Vec<Erc1155Event>> {
+    let block_number = log.block_number.map(|b| b.as_u64());
+    let address = log.address;
+    let transaction_hash = log.transaction_hash;
+    let operator = H160::from(log.topics[1]);
+    let from = H160::from(log.topics[2]);
+    let to = H160::from(log.topics[3]);
+
+    // owner_list: token_id_list
+    // from: 1
+    // from: 2
+    // from: 3
+    // to: 1
+    // to: 2
+    // to: 3
+    let (token_ids, amounts) = get_ids_and_amounts(&log.data);
+    let mut owner_list = vec![];
+    let mut token_id_list = vec![];
+    for i in 0..token_ids.len() {
+        let token_id = token_ids[i];
+        owner_list.push(from);
+        token_id_list.push(token_id);
+    }
+    for i in 0..token_ids.len() {
+        let token_id = token_ids[i];
+        owner_list.push(to);
+        token_id_list.push(token_id);
+    }
+    let balances = client.get_erc1155_balances(&address, &owner_list, &token_id_list, block_number).await?;
+
+    //
+    let mut events = vec![];
+    for i in 0..token_ids.len() {
+        let token_id = token_ids[i];
+        let amount = amounts[i];
+        // let balance_of_from = U256::zero();// balances[i];
+        // let balance_of_to = U256::zero();// balances[i+3];
+        let balance_of_from = balances[i];
+        let balance_of_to = balances[i+3];
+        events.push(Erc1155Event {
+            block_number,
+            address,
+            transaction_hash,
+            operator,
+            from,
+            balance_of_from,
+            to,
+            balance_of_to,
+            token_id, 
+            amount,
+        });
     }
 
     Ok(events)
@@ -84,22 +170,6 @@ fn get_ids_and_amounts(data: &Bytes) -> (Vec<U256>, Vec<U256>) {
     }
 }
 
-fn build_event(log: &Log, token_id: U256, amount: U256) -> Erc1155Event {
-    let operator = H160::from(log.topics[1]);
-    let from = H160::from(log.topics[2]);
-    let to = H160::from(log.topics[3]);
-    Erc1155Event {
-        block_number: log.block_number.map(|b| b.as_u64()),
-        address: log.address,
-        transaction_hash: log.transaction_hash,
-        operator,
-        from,
-        to,
-        token_id,
-        amount,
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -113,6 +183,6 @@ mod tests {
         let events = get_erc1155_events(&client, 13015344, 13015344)
             .await
             .unwrap();
-        assert_eq!(10, events.len());
+        assert_eq!(4, events.len());
     }
 }
--- libs/nft-events/src/evm_client.rs
+++ libs/nft-events/src/evm_client.rs
@@ -5,7 +5,7 @@ use array_bytes::hex2array;
 use web3::{
     contract::{Contract, Options},
     transports::http::Http,
-    types::{BlockNumber, FilterBuilder, Log, SyncState, H160, H256, U256, U64},
+    types::{BlockId, BlockNumber, FilterBuilder, Log, SyncState, H160, H256, U256, U64},
     Web3,
 };
 
@@ -342,6 +342,37 @@ impl EvmClient {
         //     Ok(None)
         // }
     }
+
+    /// Get the balance of an account's token
+    pub async fn get_erc1155_balance(&self, contract_address: &H160, owner: &H160, token_id: &U256) -> Result<U256> {
+        let contract = Contract::from_json(
+            self.web3.eth(),
+            contract_address.clone(),
+            include_bytes!("./contracts/erc1155.json"),
+        )?;
+
+        let balance: U256 = contract
+            .query("balanceOf", (owner.clone(), token_id.clone(),), None, Options::default(), None)
+            .await?;
+        Ok(balance)
+    }
+
+    /// Get the balance of multiple account/token pairs
+    pub async fn get_erc1155_balances(&self, contract_address: &H160, owners: &Vec<H160>, token_ids: &Vec<U256>, block_number: Option<u64>) -> Result<Vec<U256>> {
+        let contract = Contract::from_json(
+            self.web3.eth(),
+            contract_address.clone(),
+            include_bytes!("./contracts/erc1155.json"),
+        )?;
+
+        let block_id = block_number.map(|b| {
+            BlockId::from(U64::from(b))
+        });
+        let balances: Vec<U256> = contract
+            .query("balanceOfBatch", (owners.clone(), token_ids.clone(),), None, Options::default(), block_id)
+            .await?;
+        Ok(balances)
+    }
 }
 
 #[cfg(test)]
@@ -549,4 +580,25 @@ mod tests {
             token_uri
         );
     }
+
+    #[tokio::test]
+    async fn test_get_erc1155_balances() {
+        let web3 = Web3::new(Http::new("https://main-light.eth.linkpool.io").unwrap());
+        let client = EvmClient::new("Ethereum".to_owned(), web3);
+
+        // This erc1155 contract is not support erc1155 metadata extension, beacause supportsInterface(0x0e89341c) retruns false,
+        // but it has the uri(token_id) method
+        let address = H160::from_str("0x76be3b62873462d2142405439777e971754e8e77").unwrap();
+        let owner = H160::from_str("0x8b03428986101e8cc436eae8bf126aa81ef355b7").unwrap();
+        let owner_2 = H160::from_str("0x18ffbcdbfc165a9d6fbc4af23eb2b81942896335").unwrap();
+        let token_id = U256::from_dec_str("10106").unwrap();
+        let token_id_2 = U256::from_dec_str("10177").unwrap();
+        let balances = client
+            // .get_erc1155_balances(&address, &vec![owner], &vec![token_id], Some(13136601))
+            .get_erc1155_balances(&address, &vec![owner, owner_2], &vec![token_id, token_id_2], Some(13136601))
+            .await
+            .unwrap();
+        println!("{:?}", balances);
+    }
+
 }
